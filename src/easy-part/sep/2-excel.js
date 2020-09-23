d0d1a50e0f2e1b8eee27aac4af99e5de45638d51
jaehunn <qkdwogns98@gmail.com>
2020-09-23 20:40:10 +0900

    ...

--- src/easy-part/sep/2-excel.js
+++ src/easy-part/sep/2-excel.js
@@ -3,6 +3,25 @@
  * @param {number} n
  * @return {string}
  */
-var convertToTitle = function (n) {};
+var convertToTitle = function (n) {
+  if (n === 0) return;
+
+  let result = [];
+  while (n) {
+    let r = n % 26;
+    let d = (n / 26) << 0;
+
+    // 26 -> (0, 0) -> (-1, 26)
+    if (r === 0) {
+      r = 26;
+      d -= 1;
+    }
+
+    result.unshift(String.fromCharCode(64 + r));
+    n = d;
+  }
+
+  return result.join("");
+};
 
 console.log(convertToTitle(703)); // zzz
